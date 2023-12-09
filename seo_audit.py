9e5310b38e6ad7f8fb22e65cf882cae801a05296
smercel <sebastien.mercellus@gmail.com>
2023-12-09 19:38:06 +0100

    ajoue de étape 5

--- seo_audit.py
+++ seo_audit.py
@@ -1,4 +1,5 @@
 import csv
+from bs4 import BeautifulSoup
 
 #Retourne un dictionnaire de mots présent dans un text ainsi que ses occuerences
 # arg un texte
@@ -37,7 +38,30 @@ def lire_mots_parasites(fichier_csv):
             mots_parasites.extend(ligne)
 
     return mots_parasites
+
+def supprimer_balises_html(html):
+    # Analyser le HTML avec BeautifulSoup
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Extraire le texte du HTML
+    texte_sans_balises = soup.get_text(separator=' ', strip=True)
+
+    return texte_sans_balises
+
 #Etape 4 
 #text ="Etape 9 : Créer une fonction prenant en paramètre une chaine de caractère représentant un nom de domaine, et une liste de valeurs qui sont des url et qui retourne deux listes avec les url qui font partie du domaine et ceux qui n’en font pas partie."
 #fichier ="parasite.csv"
 #print(filtrer_mots_parasites(compter_mots_occurences(text),lire_mots_parasites(fichier)))
+
+#Test Etape 5
+#html_exemple = """
+#<html>
+ # <head>
+   # <title>Exemple HTML</title>
+  #</head>
+  #<body>
+   # <p>Ceci est un <b>exemple</b> de texte <a href="#">HTML</a>.</p>
+  #</body>
+#</html>
+#"""
+#print(supprimer_balises_html(html_exemple))
