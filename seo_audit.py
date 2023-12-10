dab56fe5c546ab5aa88fb83b2b0900eb8786f228
smercel <sebastien.mercellus@gmail.com>
2023-12-10 20:51:25 +0100

    copie du build

--- seo_audit.py
+++ seo_audit.py
@@ -13,7 +13,7 @@ def audit_page(url):
         # Compter les occurrences des mots et afficher les 3 premières
         occurrences_mots = compter_occurrences_mots(texte_sans_balises)
         print("Occurrences des mots (3 premières valeurs) :")
-        print(occurrences_mots[:3])
+        print(occurrences_mots[:3],"\n")
 
         # Lire les mots parasites depuis un fichier CSV
         fichier_parasites = "parasite.csv"
@@ -22,14 +22,14 @@ def audit_page(url):
         # Filtrer les mots parasites et afficher les occurrences restantes
         occurrences_filtrees = filtrer_mots_parasites(occurrences_mots, mots_parasites)
         print("Occurrences des mots (filtrées des mots parasites) :")
-        print(occurrences_filtrees)
+        print(occurrences_filtrees,"\n")
 
         # Extraire les liens entrants et sortants
-        liens_entrants = extraire_valeurs_balises(html, 'a', 'href')
+        liens_entrants = extraire_valeurs_balises(html, 'a', 'href'), 
         liens_sortants = []
-        print("Nombre de liens entrants :", len(liens_entrants))
+        print("Nombre de liens entrants :", len(liens_entrants),liens_entrants)
         if liens_sortants :
-            print("Nombre de liens sortants :", len(liens_sortants))
+            print("Nombre de liens sortants :", len(liens_sortants),"\n")
 
         # Vérifier la présence de balises alt dans les images
         balises_alt = extraire_valeurs_balises(html, 'img', 'alt')
@@ -37,7 +37,7 @@ def audit_page(url):
 
 if __name__ == "__main__":
     # Saisir l'URL de la page à analyser
-    page_url = input("Veuillez entrer l'URL de la page à analyser : ")
+    page_url = input("Veuillez entrer l'URL de la page à analyser : \n")
 
     # Lancer l'audit de la page
     audit_page(page_url)
